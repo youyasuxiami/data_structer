7b50e26fc72395b65b04495b960a106ff0973d64
xiami <891627097@qq.com>
2021-04-16 10:16:28 +0800

    腾讯面试题，反转单链表

--- src/com/xiami/linkedlist/SingleLinkedListDemo.java
+++ src/com/xiami/linkedlist/SingleLinkedListDemo.java
@@ -30,47 +30,51 @@ public class SingleLinkedListDemo {
         //singleLinkedList.list();
 
         //加入按照编号的顺序
-		singleLinkedList.addByOrder(hero1);
-		singleLinkedList.addByOrder(hero4);
-		singleLinkedList.addByOrder(hero2);
-		singleLinkedList.addByOrder(hero3);
-		singleLinkedList.list();
+        singleLinkedList.addByOrder(hero1);
+        singleLinkedList.addByOrder(hero4);
+        singleLinkedList.addByOrder(hero2);
+        singleLinkedList.addByOrder(hero3);
+        System.out.println("原来的链表");
+        singleLinkedList.list();
 
         //测试修改节点的代码
-        HeroNode newHeroNode = new HeroNode(2, "小卢", "玉麒麟~~");
-        singleLinkedList.update(newHeroNode);
-
-        System.out.println("修改后的链表情况~~");
-        singleLinkedList.list();
+        //HeroNode newHeroNode = new HeroNode(2, "小卢", "玉麒麟~~");
+        //singleLinkedList.update(newHeroNode);
+        //System.out.println("修改后的链表情况~~");
+        //singleLinkedList.list();
 
         //删除一个节点
-        singleLinkedList.del(1);
-        singleLinkedList.del(4);
-        System.out.println("删除后的链表情况~~");
-        singleLinkedList.list();
+        //singleLinkedList.del(1);
+        //singleLinkedList.del(4);
+        //System.out.println("删除后的链表情况~~");
+        //singleLinkedList.list();
 
         //测试一下 求单链表中有效节点的个数
-        System.out.println("有效的节点个数=" + getLength(singleLinkedList.getHead()));//2
+        //System.out.println("有效的节点个数=" + getLength(singleLinkedList.getHead()));//2
 
         //测试一下看看是否得到了倒数第K个节点
-        HeroNode res = findLastIndexNode(singleLinkedList.getHead(), 3);
-        System.out.println("res=" + res);
+        //HeroNode res = findLastIndexNode(singleLinkedList.getHead(), 3);
+        //System.out.println("res=" + res);
+
+        System.out.println("反转单链表~~");
+        reversetList(singleLinkedList.getHead());
+        singleLinkedList.list();
     }
 
     //方法：获取到单链表的节点的个数(如果是带头结点的链表，需求不统计头节点)
+
     /**
-     *
      * @param head 链表的头节点
      * @return 返回的就是有效节点的个数
      */
     public static int getLength(HeroNode head) {
-        if(head.next == null) { //空链表
+        if (head.next == null) { //空链表
             return 0;
         }
         int length = 0;
         //定义一个辅助的变量, 这里我们没有统计头节点
         HeroNode cur = head.next;
-        while(cur != null) {
+        while (cur != null) {
             length++;
             cur = cur.next; //遍历
         }
@@ -86,23 +90,49 @@ public class SingleLinkedListDemo {
     //5. 如果找到了，则返回该节点，否则返回nulll
     public static HeroNode findLastIndexNode(HeroNode head, int index) {
         //判断如果链表为空，返回null
-        if(head.next == null) {
+        if (head.next == null) {
             return null;//没有找到
         }
         //第一个遍历得到链表的长度(节点个数)
         int size = getLength(head);
         //第二次遍历  size-index 位置，就是我们倒数的第K个节点
         //先做一个index的校验
-        if(index <=0 || index > size) {
+        if (index <= 0 || index > size) {
             return null;
         }
         //定义给辅助变量， for 循环定位到倒数的index
         HeroNode cur = head.next; //3 // 3 - 1 = 2
-        for(int i =0; i< size - index; i++) {
+        for (int i = 0; i < size - index; i++) {
             cur = cur.next;
         }
         return cur;
+    }
 
+    /**
+     * 将单链表反转
+     *
+     * @param head
+     */
+    public static void reversetList(HeroNode head) {
+        //如果当前链表为空，或者只有一个节点，无需反转，直接返回
+        if (head.next == null || head.next.next == null) {
+            return;
+        }
+
+        //定义一个辅助的指针(变量)，帮助我们遍历原来的链表
+        HeroNode cur = head.next;
+        HeroNode next = null;// 指向当前节点[cur]的下一个节点
+        HeroNode reverseHead = new HeroNode(0, "", "");
+        //遍历原来的链表，每遍历一个节点，就将其取出，并放在新的链表reverseHead 的最前端
+        //动脑筋
+        while (cur != null) {
+            next = cur.next;//先暂时保存当前节点的下一个节点，因为后面需要使用
+            cur.next = reverseHead.next;//将cur的下一个节点指向新的链表的最前端
+            reverseHead.next = cur; //将cur 连接到新的链表上
+            cur = next;//让cur后移
+        }
+        //将head.next 指向 reverseHead.next , 实现单链表的反转
+        head.next = reverseHead.next;
     }
 }
 
@@ -120,15 +150,15 @@ class SingleLinkedList {
     //显示链表[遍历]
     public void list() {
         //判断链表是否为空
-        if(head.next == null) {
+        if (head.next == null) {
             System.out.println("链表为空");
             return;
         }
         //因为头节点，不能动，因此我们需要一个辅助变量来遍历
         HeroNode temp = head.next;
-        while(true) {
+        while (true) {
             //判断是否到链表最后
-            if(temp == null) {
+            if (temp == null) {
                 break;
             }
             //输出节点的信息
@@ -147,9 +177,9 @@ class SingleLinkedList {
         //因为head节点不能动，因此我们需要一个辅助遍历 temp
         HeroNode temp = head;
         //遍历链表，找到最后
-        while(true) {
+        while (true) {
             //找到链表的最后
-            if(temp.next == null) {//
+            if (temp.next == null) {//
                 break;
             }
             //如果没有找到最后, 将将temp后移
@@ -167,11 +197,11 @@ class SingleLinkedList {
         //因为单链表，因为我们找的temp 是位于 添加位置的前一个节点，否则插入不了
         HeroNode temp = head;
         boolean flag = false; // flag标志添加的编号是否存在，默认为false
-        while(true) {
-            if(temp.next == null) {//说明temp已经在链表的最后
+        while (true) {
+            if (temp.next == null) {//说明temp已经在链表的最后
                 break; //
             }
-            if(temp.next.no > heroNode.no) { //位置找到，就在temp的后面插入
+            if (temp.next.no > heroNode.no) { //位置找到，就在temp的后面插入
                 break;
             } else if (temp.next.no == heroNode.no) {//说明希望添加的heroNode的编号已然存在
 
@@ -181,7 +211,7 @@ class SingleLinkedList {
             temp = temp.next; //后移，遍历当前链表
         }
         //判断flag 的值
-        if(flag) { //不能添加，说明编号存在
+        if (flag) { //不能添加，说明编号存在
             System.out.printf("准备插入的英雄的编号 %d 已经存在了, 不能加入\n", heroNode.no);
         } else {
             //插入到链表中, temp的后面
@@ -192,7 +222,7 @@ class SingleLinkedList {
 
     public void update(HeroNode newHeroNode) {
         //判断是否空
-        if(head.next == null) {
+        if (head.next == null) {
             System.out.println("链表为空~");
             return;
         }
@@ -200,11 +230,11 @@ class SingleLinkedList {
         //定义一个辅助变量
         HeroNode temp = head.next;
         boolean flag = false; //表示是否找到该节点
-        while(true) {
+        while (true) {
             if (temp == null) {
                 break; //已经遍历完链表
             }
-            if(temp.no == newHeroNode.no) {
+            if (temp.no == newHeroNode.no) {
                 //找到
                 flag = true;
                 break;
@@ -212,7 +242,7 @@ class SingleLinkedList {
             temp = temp.next;
         }
         //根据flag 判断是否找到要修改的节点
-        if(flag) {
+        if (flag) {
             temp.name = newHeroNode.name;
             temp.nickname = newHeroNode.nickname;
         } else { //没有找到
@@ -227,11 +257,11 @@ class SingleLinkedList {
     public void del(int no) {
         HeroNode temp = head;
         boolean flag = false; // 标志是否找到待删除节点的
-        while(true) {
-            if(temp.next == null) { //已经到链表的最后
+        while (true) {
+            if (temp.next == null) { //已经到链表的最后
                 break;
             }
-            if(temp.next.no == no) {
+            if (temp.next.no == no) {
                 //找到的待删除节点的前一个节点temp
                 flag = true;
                 break;
@@ -239,10 +269,10 @@ class SingleLinkedList {
             temp = temp.next; //temp后移，遍历
         }
         //判断flag
-        if(flag) { //找到
+        if (flag) { //找到
             //可以删除
             temp.next = temp.next.next;
-        }else {
+        } else {
             System.out.printf("要删除的 %d 节点不存在\n", no);
         }
     }
@@ -254,12 +284,14 @@ class HeroNode {
     public String name;
     public String nickname;
     public HeroNode next; //指向下一个节点
+
     //构造器
     public HeroNode(int no, String name, String nickname) {
         this.no = no;
         this.name = name;
         this.nickname = nickname;
     }
+
     //为了显示方法，我们重新toString
     @Override
     public String toString() {
