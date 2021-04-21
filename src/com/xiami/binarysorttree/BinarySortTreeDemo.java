064e374311ab53897b2db43a38d5a567ec66fbb5
xiami <891627097@qq.com>
2021-04-21 08:36:46 +0800

    二叉排序树的创建和遍历

---
+++ src/com/xiami/binarysorttree/BinarySortTreeDemo.java
@@ -0,0 +1,157 @@
+package com.xiami.binarysorttree;
+
+/**
+ * Description：
+ *
+ * @version v1.0.0
+ * @author：zj
+ * @date：2021­04­21 8:26
+ */
+public class BinarySortTreeDemo {
+
+    public static void main(String[] args) {
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 2};
+        BinarySortTree binarySortTree = new BinarySortTree();
+        //循环的添加结点到二叉排序树
+        for (int i = 0; i < arr.length; i++) {
+            binarySortTree.add(new Node(arr[i]));
+        }
+
+        //中序遍历二叉排序树
+        System.out.println("中序遍历二叉排序树~");
+        binarySortTree.infixOrder(); // 1, 3, 5, 7, 9, 10, 12
+    }
+}
+
+//创建二叉排序树
+class BinarySortTree {
+    private Node root;
+
+
+    public Node getRoot() {
+        return root;
+    }
+
+    //添加结点的方法
+    public void add(Node node) {
+        if (root == null) {
+            root = node;//如果root为空则直接让root指向node
+        } else {
+            root.add(node);
+        }
+    }
+
+    //中序遍历
+    public void infixOrder() {
+        if (root != null) {
+            root.infixOrder();
+        } else {
+            System.out.println("二叉排序树为空，不能遍历");
+        }
+    }
+}
+
+//创建Node结点
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+
+        this.value = value;
+    }
+
+
+    //查找要删除的结点
+
+    /**
+     * @param value 希望删除的结点的值
+     * @return 如果找到返回该结点，否则返回null
+     */
+    public Node search(int value) {
+        if (value == this.value) { //找到就是该结点
+            return this;
+        } else if (value < this.value) {//如果查找的值小于当前结点，向左子树递归查找
+            //如果左子结点为空
+            if (this.left == null) {
+                return null;
+            }
+            return this.left.search(value);
+        } else { //如果查找的值不小于当前结点，向右子树递归查找
+            if (this.right == null) {
+                return null;
+            }
+            return this.right.search(value);
+        }
+
+    }
+    //查找要删除结点的父结点
+
+    /**
+     * @param value 要找到的结点的值
+     * @return 返回的是要删除的结点的父结点，如果没有就返回null
+     */
+    public Node searchParent(int value) {
+        //如果当前结点就是要删除的结点的父结点，就返回
+        if ((this.left != null && this.left.value == value) ||
+                (this.right != null && this.right.value == value)) {
+            return this;
+        } else {
+            //如果查找的值小于当前结点的值, 并且当前结点的左子结点不为空
+            if (value < this.value && this.left != null) {
+                return this.left.searchParent(value); //向左子树递归查找
+            } else if (value >= this.value && this.right != null) {
+                return this.right.searchParent(value); //向右子树递归查找
+            } else {
+                return null; // 没有找到父结点
+            }
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Node [value=" + value + "]";
+    }
+
+
+    //添加结点的方法
+    //递归的形式添加结点，注意需要满足二叉排序树的要求
+    public void add(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        //判断传入的结点的值，和当前子树的根结点的值关系
+        if (node.value < this.value) {
+            //如果当前结点左子结点为null
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                //递归的向左子树添加
+                this.left.add(node);
+            }
+        } else { //添加的结点的值大于 当前结点的值
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                //递归的向右子树添加
+                this.right.add(node);
+            }
+
+        }
+    }
+
+    //中序遍历
+    public void infixOrder() {
+        if (this.left != null) {
+            this.left.infixOrder();
+        }
+        System.out.println(this);
+        if (this.right != null) {
+            this.right.infixOrder();
+        }
+    }
+
+}
